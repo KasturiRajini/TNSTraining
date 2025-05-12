651f6b1dee563ff07d67b52478bf4003cc0b96bd
KasturiRajini <kasturirajini87@gmail.com>
2025-05-12 15:37:17 +0530

    done with one jdbc excercise program!

---
+++ com/tnsif/jdbc/PerformingcurdOperations.java
@@ -0,0 +1,32 @@
+package com.tnsif.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class PerformingcurdOperations {
+
+	public static void main(String[] args) {
+		try {
+			//step 1 is to register or load the driver
+			Class.forName("org.postgresql.Driver");//DriverClassName of postgresql
+			System.out.println("Registered the driver sucessfully");
+			//step 2 is to get the connection 
+			Connection connection = DriverManager.
+					getConnection("jdbc:postgresql://localhost:5432/KPRIT","postgres","root");		
+			System.out.println("Connected to the driver sucessfully");
+			//step 3 is to create a statement object 
+			String query = "insert into student values(3,'Srinivas',200)";
+			Statement stmt = connection.createStatement();
+			//step 4 is to execute the query 
+			ResultSet rs = stmt.executeQuery(query);
+			System.out.println("data inserted sucessfully");
+			//step 5 close the connection
+			connection.close();
+		}catch(Exception exc) {
+			exc.getStackTrace();
+		}
+	}
+
+}
